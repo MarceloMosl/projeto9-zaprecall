d5ea63a9367fc9efe40d6c073cc98373d0dff75b
MarceloMosl <marcelomosl@icloud.com>
2022-11-30 23:35:06 -0300

    Requisito: virar a carta com uma das perguntas, requisito: ao clicar na imagem de virar a carta novamente mostra a resposta

--- src/components/flashCards.js
+++ src/components/flashCards.js
@@ -2,21 +2,42 @@ import cards from "./card";
 import styled from "styled-components";
 import React from "react";
 function FlashCards({ viraCarta }) {
-const [teste, setTeste] = React.useState([])
+  const [cardClicked, setcardClicked] = React.useState([]);
+  const [answer, setAnswer] = React.useState("");
+  const [emJogo, setEmJogo] = React.useState("")
 
-  function viraCarta(obj, key){
-            setTeste([obj])
-            console.log(teste)
-            console.log(cards)
-            console.log(cards.includes(...teste))
+
+  function viraCarta(obj) {
+    setcardClicked([obj]);
+    setEmJogo("sim")
   }
 
-  return (cards.map((a, b) => (teste[0] == cards[b] ? <PerguntaAberta> {cards[b].question} <ion-icon name="planet-outline"></ion-icon></PerguntaAberta> : <Card key={b}><p>{`Pergunta ${b + 1}`}</p><ion-icon onClick={() => viraCarta(a, b)} name="play-outline"></ion-icon></Card>) )
-  )
-
-
-
+  function respostaQuesta(obj){
+    setAnswer(obj.answer)
   }
+  function acertei(){
+    setAnswer("")
+    setcardClicked([])
+    setEmJogo("")
+  }
+
+  const PerguntaAberta = (a,b) => (
+    <StylePerguntaAberta>
+      {answer == "" ? cardClicked[0].question : answer}
+      {answer == "" ? <ion-icon onClick={()=>respostaQuesta(a)} name="planet-outline"></ion-icon> : <div><button onClick={acertei}>acertei</button> <button>não lembrei</button> <button>lembrei +-</button></div> }
+      </StylePerguntaAberta>
+      );
+  const PerguntaFechada = (a,b) => (
+    <Card key={b}>
+      <p>{`Pergunta ${b + 1}`}</p>
+      <ion-icon onClick={emJogo == "" ? () => viraCarta(a): ""} name="play-outline"> </ion-icon>
+    </Card>
+      );
+
+
+  return cards.map((a, b) => cardClicked[0] === cards[b] ? PerguntaAberta(a,b) : PerguntaFechada(a,b)
+  );
+}
 const Card = styled.div`
   width: 300px;
   height: 35px;
@@ -40,15 +61,15 @@ const Card = styled.div`
     font-size: 30px;
   }
 `;
-const PerguntaAberta = styled.div`
-width: 300px;
+const StylePerguntaAberta = styled.div`
+  width: 300px;
   margin: 12px;
   padding: 15px;
   min-height: 100px;
-  background: #FFFFD5;
+  background: #ffffd5;
   box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
   border-radius: 5px;
-  font-family: 'Recursive';
+  font-family: "Recursive";
   font-style: normal;
   font-weight: 400;
   font-size: 18px;
@@ -58,5 +79,11 @@ width: 300px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-`
+  ion-icon {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+  }
+`;
+
 export default FlashCards;
